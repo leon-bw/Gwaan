ec94bdeea1c196e41eeb8f62e0c02b7f557babbf
Leon Williams <leonbwilliams6@gmail.com>
2024-09-24 16:55:07 +0100

    Refactor banner component

--- components/banner.tsx
+++ components/banner.tsx
@@ -5,37 +5,28 @@ import BSALogo from "@/assets/BSA Logo_Colour_PNG.png";
 import CampusLogo from "@/assets/campusfounders.png";
 import Image from "next/image";
 
+const logos = [
+  { src: IUKLogo, alt: "Innovate UK Logo" },
+  { src: MSFoundersLogo, alt: "MS Founders Logo" },
+  { src: BarclaysLogo, alt: "Barclays Eagle Labs Logo" },
+  { src: BSALogo, alt: "British Science Association Logo" },
+  { src: CampusLogo, alt: "Campus Founders Logo" },
+];
+
 const Banner = () => {
   return (
     <section className="py-4 md:py-8 bg-white">
       <div className="container max-w-screen-2xl mx-auto">
         <div className="flex  items-center justify-center overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
           <div className="flex gap-14 flex-none">
-            <Image
-              src={IUKLogo}
-              alt="Innovate UK Logo"
-              className="banner-image"
-            />
-            <Image
-              src={MSFoundersLogo}
-              alt="MS Founders Logo"
-              className="banner-image"
-            />
-            <Image
-              src={BarclaysLogo}
-              alt="Barclays Eagle Labs Logo"
-              className="banner-image"
-            />
-            <Image
-              src={BSALogo}
-              alt="British Science Association Logo"
-              className="banner-image"
-            />
-            <Image
-              src={CampusLogo}
-              alt="Campus Founders Logo"
-              className="banner-image"
-            />
+            {logos.map((logo, index) => (
+              <Image
+                key={index}
+                src={logo.src}
+                alt={logo.alt}
+                className="banner-image"
+              />
+            ))}
           </div>
         </div>
       </div>
