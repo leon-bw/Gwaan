473b18136bfe87a8f53b4d5d3b2a851ba6e77914
Leon Williams <leonbwilliams6@gmail.com>
2024-10-01 14:35:53 +0100

    Refactor product showcase component

--- components/product-showcase.tsx
+++ components/product-showcase.tsx
@@ -1,6 +1,4 @@
-import productImage from "@/assets/iPhone_mockup.png";
-import workout from "@/assets/exercise_session_short.gif";
-import Image from "next/image";
+import Product from "@/components/product";
 
 const ProductShowcase = () => {
   return (
@@ -16,16 +14,7 @@ const ProductShowcase = () => {
             providing instant feedback so you can focus on what really matters
             and maximize your results!
           </p>
-
-          <div className="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[586px] w-[341px] shadow-xl mt-10">
-            <div className="w-[148px] h-[18px] bg-gray-800 top-0 rounded-b-[1rem] left-1/2 -translate-x-1/2 absolute"></div>
-            <div className="h-[46px] w-[3px] bg-gray-800 absolute -start-[17px] top-[124px] rounded-s-lg"></div>
-            <div className="h-[46px] w-[3px] bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
-            <div className="h-[64px] w-[3px] bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
-            <div className="rounded-[2rem] overflow-hidden w-[313px] h-[558px] bg-white dark:bg-gray-800">
-              <Image src={workout} alt="Workout using Gwaan AI" />
-            </div>
-          </div>
+          <Product />
         </div>
       </div>
     </section>
