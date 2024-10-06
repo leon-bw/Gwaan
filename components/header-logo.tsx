12320b7deee8b8f50be4e44e72d611757a9d485f
Leon Williams <leonbwilliams6@gmail.com>
2024-10-06 16:03:16 +0100

    Fix the size of logo on mobile and desktop devices

--- components/header-logo.tsx
+++ components/header-logo.tsx
@@ -5,8 +5,8 @@ import Logo from "@/assets/Gwaan_logo.png";
 const HeaderLogo = () => {
   return (
     <Link href="/">
-      <div className="items-center lg:flex">
-      <Image src={Logo} alt="Gwaan Logo" height={24} />
+      <div className="w-16 items-center lg:flex lg:w-24">
+        <Image src={Logo} alt="Gwaan Logo" />
       </div>
     </Link>
   );
