6a257472f1ccde57297b15e4e99eaf3cb0e81ae5
Leon Williams <leonbwilliams6@gmail.com>
2024-10-25 19:53:50 +0100

    Add logo, fix styling and add icons to menu buttons

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -5,29 +5,45 @@ import { useMedia } from "react-use";
 import { usePathname, useRouter } from "next/navigation";
 import NavButton from "@/components/navbutton";
 import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import HeaderLogo from "@/components/header-logo";
 import { Menu } from "lucide-react";
+import { Home11Icon } from "hugeicons-react";
+import { Agreement02Icon } from "hugeicons-react";
+import { LicenseDraftIcon } from "hugeicons-react";
+import { InformationCircleIcon } from "hugeicons-react";
+import { MessageMultiple02Icon } from "hugeicons-react";
 
 const routes = [
   {
     href: "/",
     label: "Home",
+    icon: Home11Icon,
   },
   {
     href: "/partner",
     label: "Partner with Us",
+    icon: Agreement02Icon,
   },
   {
     href: "/blog",
     label: "Blog",
+    icon: LicenseDraftIcon,
   },
   {
     href: "/about",
     label: "About",
+    icon: InformationCircleIcon,
   },
   {
     href: "/contact-us",
     label: "Contact Us",
+    icon: MessageMultiple02Icon,
   },
 ];
 
@@ -54,18 +70,26 @@ const Navigation = () => {
             <Menu className="size-5" />
           </Button>
         </SheetTrigger>
-        <SheetContent side="right" className="px-2 bg-night border-none">
-          <nav className="flex flex-col gap-y-2 pt-6">
-            {routes.map((route) => (
-              <Button
-                key={route.href}
-                variant={route.href === pathname ? "primary" : "ghost"}
-                onClick={() => onClick(route.href)}
-                className="w-full text-white hover:bg-black hover:text-white"
-              >
-                {route.label}
-              </Button>
-            ))}
+        <SheetContent side="right" className="px-6 bg-night border-none pt-8">
+          <SheetTitle>
+            <HeaderLogo />
+          </SheetTitle>
+          <nav className="flex flex-col gap-y-4 pt-12">
+            {routes.map((route) => {
+              const NavIcons = route.icon;
+              return (
+                <Button
+                  key={route.href}
+                  variant={route.href === pathname ? "primary" : "ghost"}
+                  onClick={() => onClick(route.href)}
+                  className="w-full text-white hover:bg-black hover:text-white hover:translate-x-2 transition duration-300 font-semibold text-sm justify-start pl-4"
+                  size="lg"
+                >
+                  <NavIcons className="mr-4" size={24} />
+                  {route.label}
+                </Button>
+              );
+            })}
           </nav>
         </SheetContent>
       </Sheet>
