8f377d01bb8443f80314f763773f90eaf7c3d8c4
Leon Williams <leonbwilliams6@gmail.com>
2025-06-05 17:03:13 +0100

    Update prisma client configuration

--- lib/db.ts
+++ lib/db.ts
@@ -3,9 +3,15 @@ import { PrismaClient } from '@prisma/client'
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error'] : ['error'],
+    
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL
+      }
+    }
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
